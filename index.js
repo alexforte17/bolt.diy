fc3acac4ea9575b484b87340965043f2ed38f560
alexforte17 <at@goodsmartidea.com>
2025-01-13 18:15:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ export default {
     return worker.fetch(req);
   }
 };
+
+
+
+export class MyDurableObject {
+  constructor(state, env) {
+    this.state = state;
+    this.env = env;
+  }
+
+  async fetch(request) {
+    return new Response('Hello from MyDurableObject!');
+  }
+}
+export default {
+  async fetch(req, env) {
+    const worker = env.dispatcher.get("my-github-worker");
+    return worker.fetch(req);
+  },
+};
+
+export { MyDurableObject };
