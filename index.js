85de171d28c2a3878654742f4a6c2a33dbc168c5
alexforte17 <at@goodsmartidea.com>
2025-01-13 18:16:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-export default {
-  async fetch(req, env) {
-    const worker = env.dispatcher.get("my-github-worker");
-    return worker.fetch(req);
-  }
-};
-
-
-
 export class MyDurableObject {
   constructor(state, env) {
     this.state = state;
@@ -17,6 +8,7 @@ export class MyDurableObject {
     return new Response('Hello from MyDurableObject!');
   }
 }
+
 export default {
   async fetch(req, env) {
     const worker = env.dispatcher.get("my-github-worker");
